3bf7e19218ad90d366054f2632825186173eff1a
shauek97 <shauek97@gmail.com>
2023-05-23 14:12:00 +0200

    Completed headers layout

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ export default function App() {
   return (
     <>
     <header className="header">
-      <div className="background-image">
+      
       <nav>
           <div className="container">
+            <div className='navbar'>
                 <div className="logo">
-                <img src='https://i.postimg.cc/T33bVnpt/the-hicker-black.png' border='0' alt='the-hicker-black'/>
+                  <img src='https://i.postimg.cc/T33bVnpt/the-hicker-black.png' border='0' alt='the-hicker-black'/>
                 </div>                
-                <div className="title">
-                  <h1>When the mountains meets programming</h1>
-                </div>                
+                <div className="menu">
+                  <h2>Home</h2>
+                  <h2>About</h2>
+                  <h2>Blog</h2>
+                  <h2>GitHub</h2>
+                  <h2>Contact</h2>
+                </div>
+            </div>   
           </div>
-        </nav>
-      </div>
-      
+      </nav>
     </header>
     <main>
-      <section></section>
-      <article>
-
-      </article>
-      <section></section>
+      <section>
+        <div className="background-image">
+          <div className="container">
+            <div className="title">
+                <h1>WHEN MOUNTAINS<br></br> MEETS PROGRAMMING</h1>
+            </div>    
+            <div className="btn">About</div>
+          </div>
+        </div>
+      </section>
+      <section className='about'>
+        
+      </section>
     </main>
     <footer>
 

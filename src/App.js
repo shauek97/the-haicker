212d02a80c5ff647c15106a6bb1293d904331c0b
shauek97 <shauek97@gmail.com>
2023-05-25 15:56:03 +0200

    Finished layout of skills and projects

--- src/App.js
+++ src/App.js
@@ -123,10 +123,27 @@ export default function App() {
           </div>
           <div className='projects-list'>
             <div className='project-box'>
-              <div className='project-title'></div>
-              <div className='project-description'></div>
+              <div className='project-title'><h5>Waiter App</h5></div>
+              <div className='project-description'><p>A react application that allows to make reservation of table in restaurant</p></div>
+            </div>
+            <div className='project-box'>
+              <div className='project-title'><h5>Calculator</h5></div>
+              <div className='project-description'><p>Basic Calculator made in vanilla javascript</p></div>
+            </div>
+            <div className='project-box'>
+              <div className='project-title'><h5>Tic tac toe</h5></div>
+              <div className='project-description'><p>A tic tac toe pvp game made in vanilla javascript</p></div>
+            </div>
+            <div className='project-box'>
+              <div className='project-title'><h5>Paper rock scissors</h5></div>
+              <div className='project-description'><p>A paper, rock, scissors game made in vanilla javascript</p></div>
+            </div>
+            <div className='ps'>
+            <p>For more projects do step into my <div className="github">GitHub.</div></p>
             </div>
           </div>
+          
+
         </div>
       </section>
     </main>

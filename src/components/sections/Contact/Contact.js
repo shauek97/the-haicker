9d8560f29e45609d512e1d1157e3def725b9fc00
shauek97 <shauek97@gmail.com>
2023-05-30 08:06:10 +0200

    Started contact form styling

--- src/components/sections/Contact/Contact.js
+++ src/components/sections/Contact/Contact.js
@@ -8,13 +8,14 @@ export default function Contact() {
             <div className={styles.contactTitle}>
             <h4>CONTACT <span>me</span></h4>
             </div>
-
-            <form action="https://formsubmit.co/e74bb1e61458f78724df9c5d8eee68fb" method="POST">
-            <input type="text" name="name" placeholder='Name' required></input>
-            <input type="email" name="email" placeholder='Email' required></input>
-            <textarea name="content" placeholder="What's up?" required></textarea>
-            <button type="submit">Send</button>
-            </form>
+            <div className={styles.form}>
+                <form action="https://formsubmit.co/e74bb1e61458f78724df9c5d8eee68fb" method="POST">
+                <input type="text" name="name" placeholder='Name' required></input>
+                <input type="email" name="email" placeholder='Email' required></input>
+                <textarea name="content" placeholder="What's up?" required></textarea>
+                <button type="submit">Send</button>
+                </form>
+            </div>
 
         </section>
     );

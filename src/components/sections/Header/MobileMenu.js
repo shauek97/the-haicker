bed9dd3eda0e01dddcc1178293dc8f5394ca5ff1
shauek97 <shauek97@gmail.com>
2023-06-05 23:02:05 +0200

    Added hamburger icon

--- src/components/sections/Header/MobileMenu.js
+++ src/components/sections/Header/MobileMenu.js
@@ -1,13 +1,23 @@
 import styles from "./Header.module.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const barsIcon = <FontAwesomeIcon icon={faBars} />;
+
 export default function MobileMenu({ getToContact, getToAbout }) {
   return (
-    <div className={styles.mobileMenu}>
-      <h2>Home</h2>
-      <h2 onClick={getToAbout}>About</h2>
-      <h2>Blog</h2>
-      <h2>GitHub</h2>
-      <h2 onClick={getToContact}>Contact</h2>
-    </div>
+    <>
+      <div className={styles.hamburgerBox}>
+      <div className={styles.hamburger}>{barsIcon}</div>
+      </div>
+      <div className={styles.mobileMenu}>
+        <h2>Home</h2>
+        <h2 onClick={getToAbout}>About</h2>
+        <h2>Blog</h2>
+        <h2>GitHub</h2>
+        <h2 onClick={getToContact}>Contact</h2>
+      </div>
+    </>
   );
 }

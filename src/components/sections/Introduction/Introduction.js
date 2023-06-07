cb10f079cce70fc75ca572f98244fd4ce16cdb10
shauek97 <shauek97@gmail.com>
2023-06-07 17:58:46 +0200

    Completed introduction responsivness

--- src/components/sections/Introduction/Introduction.js
+++ src/components/sections/Introduction/Introduction.js
@@ -1,29 +1,37 @@
-import styles from './Introduction.module.scss'
-import Container from '../../architecture/Container/Container';
+import styles from "./Introduction.module.scss";
+import Container from "../../architecture/Container/Container";
 
-export default function Header(){
-    return(
+export default function Header() {
+  return (
     <>
-        <section id="introduction" className={styles.faceHint}>
-            <Container>
-                <div className={styles.mainBox}>
-                    <div className={styles.descriptionBox}>
-                        <h3>My name is Wojciech Szałkiewicz and I am simple guy from Poland, which wants to bring mountains and programming to one place.</h3>
-                    </div>
-                    <div className={styles.imageBox}>
-                        <img src='https://i.postimg.cc/CMZ2hcfM/cvphoto.png' border='0' alt='cvphoto' />
-                    </div>
-                </div>
-            </Container>
-
-        </section>
-        <section className={styles.introduction}>
-                <div className={styles.textHolder}>
-                    <p>
-                        My main vision of this website is to create programming portfolio and blog, where I will write down every most important milestone of my programming and mountain hiking progress.
-                    </p>
-                </div>
-            </section>
+      <section id="introduction" className={styles.faceHint}>
+        <Container>
+          <div className={styles.mainBox}>
+          <div className={styles.imageBox}>
+              <img
+                src="https://i.postimg.cc/CMZ2hcfM/cvphoto.png"
+                border="0"
+                alt="cvphoto"
+              />
+            </div>
+            <div className={styles.descriptionBox}>
+              <p>
+                My name is Wojciech Szałkiewicz and I am simple guy from Poland,
+                which wants to bring mountains and programming to one place.
+              </p>
+            </div>
+          </div>
+        </Container>
+      </section>
+      <section className={styles.introduction}>
+        <div className={styles.textHolder}>
+          <p>
+            My main vision of this website is to create programming portfolio
+            and blog, where I will write down every most important milestone of
+            my programming and mountain hiking progress.
+          </p>
+        </div>
+      </section>
     </>
-    );
+  );
 }
--- src/components/sections/WelcomeSection/WelcomeSection.js
+++ src/components/sections/WelcomeSection/WelcomeSection.js
@@ -1,27 +1,30 @@
-import styles from './WelcomeSection.module.scss';
-import Container from '../../architecture/Container/Container';
-import { CSSTransition } from 'react-transition-group';
-import { useState } from 'react';
+import styles from "./WelcomeSection.module.scss";
+import Container from "../../architecture/Container/Container";
+import { CSSTransition } from "react-transition-group";
+import { useState } from "react";
 
 export default function App() {
-
   const handleClickScroll = () => {
-    const element = document.getElementById('introduction');
+    const element = document.getElementById("introduction");
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
-  }
+  };
 
-    return (
-        <section>
-          <div className={styles.backgroundImage}>          
-            <Container>
-              <div className={styles.title}>
-                  <h1>WHEN MOUNTAINS<br></br> MEETS PROGRAMMING</h1>
-              </div>    
-              <div className={styles.btn} onClick={handleClickScroll}>About</div>
-            </Container>
+  return (
+    <section>
+      <div className={styles.backgroundImage}>
+        <Container>
+          <div className={styles.title}>
+            <h1>
+              WHEN MOUNTAINS<br></br> MEETS PROGRAMMING
+            </h1>
           </div>
-        </section>
-    );
+          <div className={styles.btn} onClick={handleClickScroll}>
+            About
+          </div>
+        </Container>
+      </div>
+    </section>
+  );
 }
